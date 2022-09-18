a91f9a63b636da0c87d33eb56413ee1d2499b5e4
Enshen Zhu <winniecmz.zhu@gmail.com>
2022-09-18 11:03:42 -0400

    finish the learning of the basic printing

---
+++ 0_helloWorld/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl; // endl stands for ending the line
+    return 0;
+}
---
+++ 1_drawing_a_shape/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "----" << endl;
+    cout << "----" << endl;
+    cout << "----" << endl;
+
+    // the following two lines of code will print on a single line
+    cout << "----";
+    cout << "----";
+
+    return 0;
+}
